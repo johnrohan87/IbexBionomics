0581df43f61394f9339a1112cf8ae0c4ea87c3e8
John Rohan <john.rohan87@gmail.com>
2022-03-28 15:40:50 -0400

    added new pdf and rearagned home page

--- packages/landing-gatsby/src/containers/Interior/HomePage/Catalog/index.js
+++ packages/landing-gatsby/src/containers/Interior/HomePage/Catalog/index.js
@@ -28,11 +28,13 @@ import leafbg from '../../../../common/assets/image/interior/IbexImages/S2/LeafB
 import hpAquaculture from '../../../../common/assets/image/interior/IbexImages/HomePage/HPAquaculture.jpg'
 import hpHydrocarbon from '../../../../common/assets/image/interior/IbexImages/HomePage/HPHydrocarbon.jpg'
 import MicroorganismsBG from '../../../../common/assets/image/interior/IbexImages/S3/S3MicroorganismsBG.jpg'
+import nu3zer from '../../../../common/assets/image/interior/IbexImages/HomePage/HPNu3zer.jpg'
 
 import Agriculture_Catalogue from '../../../../common/assets/PDFs/Agriculture_Catalogue_ENG.pdf'
 import Aquaculture_Catalog from '../../../../common/assets/PDFs/Aquaculture_Catalog.pdf'
 import Hydrocarbon_Catalog from '../../../../common/assets/PDFs/Hydrocarbon_Catalog.pdf'
 import Waste_Waters_Catalogue from '../../../../common/assets/PDFs/Waste_Waters_Catalogue.pdf'
+import Nu3zer_Catalog from '../../../../common/assets/PDFs/Nu3zer_Catalog.pdf'
 
 const Catalog = (props) => {
   const [currentPDF,setCurrentPDF] = useState('none')
@@ -71,7 +73,7 @@ const Catalog = (props) => {
         <Container>
           <div className="flex h100 alignCenter">
             <ContentArea minWidth="25vw" minHeight="60vh" backgroundImage={`url(${leafbg})`} >
-              <p className="centerXandY whiteText cursorPointer" onClick={()=>setCurrentPDF(Agriculture_Catalogue)}>
+              <p className="centerXandY whiteText cursorPointer" onClick={()=> currentPDF === Agriculture_Catalogue ? setCurrentPDF('none'): setCurrentPDF(Agriculture_Catalogue)}>
                     <b>
                     Agriculture
                     </b>
@@ -79,21 +81,26 @@ const Catalog = (props) => {
                 </a>*/}
               </p>
               <div className="Agriculture_Catalogue">
-              { isNarrowScreen ? (currentPDF == Agriculture_Catalogue ? <><PDFDisplay pdf={currentPDF} /></> : <></>) : <></>}
+              { isNarrowScreen ? (currentPDF === Agriculture_Catalogue ? <><PDFDisplay pdf={currentPDF} /></> : <></>) : <></>}
               </div>
             </ContentArea>
             <ContentArea minWidth="25vw" minHeight="60vh" backgroundImage={`url(${hpAquaculture})`}>
-              <p className="centerXandY whiteText cursorPointer" onClick={()=>setCurrentPDF(Aquaculture_Catalog)}>
+              <p className="centerXandY whiteText cursorPointer" onClick={()=> currentPDF === Aquaculture_Catalog ? setCurrentPDF('none'): setCurrentPDF(Aquaculture_Catalog)}>
                 <b>
                 Aquaculture
                 </b>
               {/** <a className="whiteText" href={Aquaculture_Catalog} download>
               </a> */}
               </p>
-              { isNarrowScreen ? (currentPDF == Aquaculture_Catalog ? <PDFDisplay pdf={currentPDF} /> : "") : ""}
+              { isNarrowScreen ? (currentPDF === Aquaculture_Catalog ? <PDFDisplay pdf={currentPDF} /> : "") : ""}
             </ContentArea>
+          </div>
+          {isNarrowScreen ? "" : currentPDF === "none"  ? "" : currentPDF === Agriculture_Catalogue || currentPDF === Aquaculture_Catalog ? <PDFDisplay pdf={currentPDF} />:""}
+        </Container>
+        <Container>
+          <div className="flex h100 alignCenter">
             <ContentArea minWidth="25vw" minHeight="60vh" backgroundImage={`url(${hpHydrocarbon})`}>
-              <p className="centerXandY whiteText cursorPointer" onClick={()=>setCurrentPDF(Hydrocarbon_Catalog)}>
+              <p className="centerXandY whiteText cursorPointer" onClick={()=> currentPDF === Hydrocarbon_Catalog ? setCurrentPDF('none'): setCurrentPDF(Hydrocarbon_Catalog)}>
                   <b>
                   Environmental Remediation
                   <br/>
@@ -102,10 +109,10 @@ const Catalog = (props) => {
               {/** <a className="whiteText" href={Hydrocarbon_Catalog} download>
               </a> */}
                 </p>
-                { isNarrowScreen ? (currentPDF == Hydrocarbon_Catalog ? <PDFDisplay pdf={currentPDF} /> : "") : ""}
+                { isNarrowScreen ? (currentPDF === Hydrocarbon_Catalog ? <PDFDisplay pdf={currentPDF} /> : "") : ""}
             </ContentArea>
             <ContentArea minWidth="25vw" minHeight="60vh" backgroundImage={`url(${MicroorganismsBG})`}>
-                <p className="centerXandY whiteText cursorPointer" onClick={()=>setCurrentPDF(Waste_Waters_Catalogue)}>
+                <p className="centerXandY whiteText cursorPointer" onClick={()=> currentPDF === Waste_Waters_Catalogue ? setCurrentPDF('none'): setCurrentPDF(Waste_Waters_Catalogue)}>
                   <b>
                   Environmental Remediation
                   <br/>
@@ -114,10 +121,27 @@ const Catalog = (props) => {
               {/** <a className="whiteText" href={Waste_Waters_Catalogue} download>
               </a> */}
                 </p>
-                { isNarrowScreen ? (currentPDF == Waste_Waters_Catalogue ? <PDFDisplay pdf={currentPDF} /> : "") : ""}
+                { isNarrowScreen ? (currentPDF === Waste_Waters_Catalogue ? <PDFDisplay pdf={currentPDF} /> : "") : ""}
             </ContentArea>
           </div>
-          {isNarrowScreen ? "" : currentPDF == "none" ? "" : <PDFDisplay pdf={currentPDF} />}
+          {isNarrowScreen ? "" : currentPDF === "none" ? "" : currentPDF === Hydrocarbon_Catalog || currentPDF === Waste_Waters_Catalogue ? <PDFDisplay pdf={currentPDF} />:""}
+        </Container>
+        <Container>
+          <div className="flex h100 alignCenter">
+            <ContentArea minWidth="25vw" minHeight="60vh" backgroundImage={`url(${nu3zer})`} >
+              <p className="centerXandY whiteText cursorPointer" onClick={()=> currentPDF === Nu3zer_Catalog ? setCurrentPDF('none'): setCurrentPDF(Nu3zer_Catalog)}>
+                    <b>
+                    Nu3zer
+                    </b>
+                {/*<a className="whiteText" href={Agriculture_Catalogue} download>
+                </a>*/}
+              </p>
+              <div className="Agriculture_Catalogue">
+              { isNarrowScreen ? (currentPDF === Nu3zer_Catalog ? <><PDFDisplay pdf={currentPDF} /></> : <></>) : <></>}
+              </div>
+            </ContentArea>
+          </div>
+          {isNarrowScreen ? "" : currentPDF === "none" ? "" : currentPDF === Nu3zer_Catalog ? <PDFDisplay pdf={currentPDF} />:""}
         </Container>
       </BannerWrapper>
     </div>
